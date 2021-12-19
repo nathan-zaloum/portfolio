036498f37258df78c06d96a815970a5bad878f6c
nathan-zaloum <51972215+nathan-zaloum@users.noreply.github.com>
2021-12-20 11:16:26 +1300

    Minor changes to portfolio texts

--- client/utils/web-dictionary.js
+++ client/utils/web-dictionary.js
@@ -5,9 +5,14 @@ export const titles = [
 ]
 
 export const descriptions = [
-    [`A week long project I worked on with 3 others at Dev Academy.`],
-    [`A project I made to help teach myself Korean.`],
-    [`My portfolio website I made to showcase my work.`]
+    [`A week long project I worked on with 3 others at Dev Academy.`,
+     `Making a game with React was an exciting challenge that made me so much more comfortable with the tech.`,
+     `Play as Jake the Snake as you make your own dungeons to explore and play!`],
+    [`A project I made to help teach myself Korean.`,
+     `This project taught me lots about responsiveness and even more about the structure of hangul.`,
+     `Type out the hangul blocks correctly to score points.`],
+    [`My portfolio website I made to showcase my work. You happen to be exploring it already!`,
+     `My aim was to make a fully responsive site with a dark/light mode as well.`]
 ]
 
 export const images = [
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -656,10 +656,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "buttons": () => (/* binding */ buttons)
 /* harmony export */ });
 var titles = ['Dungeon Slither', 'Hangul', 'Portfolio Website'];
-var descriptions = [["A week long project I worked on with 3 others at Dev Academy."], ["A project I made to help teach myself Korean."], ["My portfolio website I made to showcase my work."]];
+var descriptions = [["A week long project I worked on with 3 others at Dev Academy.", "Making a game with React was an exciting challenge that made me so much more comfortable with the tech.", "Play as Jake the Snake as you make your own dungeons to explore and play!"], ["A project I made to help teach myself Korean.", "This project taught me lots about responsiveness and even more about the structure of hangul.", "Type out the hangul blocks correctly to score points."], ["My portfolio website I made to showcase my work. You happen to be exploring it already!", "My aim was to make a fully responsive site with a dark/light mode as well."]];
 var images = ['images/portfolio/dungeon-slither.png', 'images/portfolio/hangul.png', 'images/portfolio/portfolio.png'];
-var links = ['https://dungeon-slither.herokuapp.com', 'https://hangul-nathan.herokuapp.com', 'https://nathan-portfolio.herokuapp.com'];
-var buttons = ['dungeon-slither.herokuapp.com', 'hangul-nathan.herokuapp.com', 'nathan-portfolio.herokuapp.com'];
+var links = ['https://dungeon-slither.herokuapp.com', 'https://hangul-nathan.herokuapp.com', 'https://nathanzaloum.herokuapp.com'];
+var buttons = ['dungeon-slither.herokuapp.com', 'hangul-nathan.herokuapp.com', 'nathanzaloum.herokuapp.com'];
 
 /***/ }),
 
